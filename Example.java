b94c3a8db76abf48925a030307da0b4664b86c1e
amanpreetkaur <amandharna19oct@gmail.com>
2021-01-27 14:15:43 +0530

    ok

---
+++ Example.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.*;
+class Example {
+	int x,y;
+	void show() {
+		System.out.println("Hello World");
+	}
+}
+
+class RunApp {
+	public static void main(String[] args) {
+		Example e = new Example();
+		Class c1 = e.getClass();
+		Method m[] = c1.getDeclaredMethods();
+
+		for(Method method : m) {
+			System.out.println(method.getName());
+		}
+
+		Field f[] = c1.getDeclaredFields();
+
+		for(Field field : f) {
+			System.out.println(field.getName());
+		}
+
+		e.show();
+		System.out.println(c1.getName());
+	}	
+}
+	
